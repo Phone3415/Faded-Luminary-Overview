20dad6368ff8fc931673cffba9e4b9467486d117
Phone3415 <nleisxudm@gamil.com>
2025-12-19 01:26:09 +0700

    polish navbar

--- static/script.js
+++ static/script.js
@@ -4,9 +4,9 @@ function main() {
   const navBtn = document.querySelector(".nav-btn");
   const nav = document.querySelector("nav");
 
-  if (navBtn.computedStyleMap().get("display").value === "none") return;
-
   function navEvent() {
+    if (navBtn.computedStyleMap().get("display").value === "none") return;
+
     nav.classList.toggle("hide");
     navBtn.classList.toggle("hide");
   }
